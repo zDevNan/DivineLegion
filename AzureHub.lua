46de105534c2f905f115281912cb182bb4114135
Renan <167133780+zDevNan@users.noreply.github.com>
2025-03-12 11:12:18 -0300

    Update AzureHub.lua

--- AzureHub.lua
+++ AzureHub.lua
@@ -1,4 +1,6 @@
-local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/FluentScripting/Fluent/main/library.lua"))()
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
     Title = "Farm Script",
