76d729ac2fcdaa6bb2760ec4b6ee8a2348a300aa
Renan <167133780+zDevNan@users.noreply.github.com>
2025-03-12 11:20:59 -0300

    Update AzureHub.lua

--- AzureHub.lua
+++ AzureHub.lua
@@ -1,4 +1,5 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
